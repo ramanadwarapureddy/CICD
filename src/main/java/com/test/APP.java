4b1cb87e7148fa23bd0438eee7299a394aad871b
ramanrepo <dvvr12@gmail.com>
2019-02-04 12:30:07 +0530

    Sysout line's to be separatedd.

--- src/main/java/com/test/APP.java
+++ src/main/java/com/test/APP.java
@@ -4,7 +4,8 @@ public class APP {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to java.");System.out.println("Welcome to java.");
+		System.out.println("Welcome to java.");
+		System.out.println("Welcome to java.");
 
 
 	}
